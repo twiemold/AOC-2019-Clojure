4f10c9b26f525e3314f2d04d351fc93f215d3499
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2024-06-29 20:35:43 -0400

    Solve day one part two.

--- day-one/src/day_one/core.clj
+++ day-one/src/day_one/core.clj
@@ -1,8 +1,7 @@
 (ns day-one.core
   (:require [clojure.math :as math]))
 
-
-(defn calc-fuel [module]
+(defn calc-fuel [module] 
   (-> (/ module 3) 
       (math/floor) 
       (- 2)
@@ -10,17 +9,33 @@
       )
   )
 
+(defn recur-fuel [module]
+  (loop [modl module
+         prior []]
+    (let [fuel (calc-fuel modl)] 
+      (if (neg? fuel) 
+        (reduce + prior) 
+        (recur fuel (conj prior fuel)))))
+  )
+
+(def memo-recur-fuel (memoize recur-fuel))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [modules (map parse-long 
                      (line-seq (clojure.java.io/reader "input.txt")))]
+    ;; part one
+    ;; (print "The sum is: " (reduce + (map calc-fuel modules))) 
     (print "The sum is: " 
-           (reduce + (map calc-fuel modules))))
+           (reduce + (map memo-recur-fuel modules))) )
   )
 
 (comment
   (-main)
+  (calc-fuel 14)
+  (map recur-fuel '(14))
+  (map recur-fuel '(1969))
   (map calc-fuel '(12 1969))
   (reduce + (map calc-fuel '(12 1969)))
   )
