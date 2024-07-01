c70ace27644a5215f0d4fda2f704fb731b90b3e8
Thomas Wiemold <thomas.wiemold@gmail.com>
2024-06-30 22:17:30 -0400

    Solve day two part two.

--- day-two/src/day_two/core.clj
+++ day-two/src/day_two/core.clj
@@ -20,13 +20,39 @@
                                 (nth prog op-loc-two))) 
                (+ curr 4))))))
 
+(defn get-noun-verb-for [value program]
+  (loop [prog program
+         target value 
+         interval [[0 0] [99 99]]
+         coords (for [x (range (dec (get-in interval [0 0])) (get-in interval [1 0])) 
+                       y (range (dec (get-in interval [0 1])) (get-in interval [1 1]))] 
+                  (seq [(inc x) (inc y)]))
+         noun (first (first coords))
+         verb (second (first coords))]
+    (let [attempt (nth (intcode-processor (assoc prog 1 noun 2 verb)) 0)]
+      (if (= attempt target)
+        (vector noun verb)
+        (recur prog 
+               target 
+               interval
+               (rest coords)
+               (first (first coords))
+               (second (first coords)))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [grav-prog (vec (map parse-long (str/split (slurp "input.txt") #",")) )]
-    (println "Position 0 is" (nth (intcode-processor (assoc grav-prog 1 12 2 2)) 0))))
+  (let [grav-prog (vec (map parse-long (str/split (slurp "input.txt") #",")) )
+        ;; part two
+        noun-verb (get-noun-verb-for 19690720 grav-prog)]
+    ;; part one
+    ;;(println "Position 0 is" (nth (intcode-processor (assoc grav-prog 1 12 2 2)) 0)) 
+    (println "Answer is:" (+ (* 100 (first noun-verb)) (second noun-verb)))
+    ))
 
 (comment 
-  (-main)
+  (-main) 
+  (nth (intcode-processor (assoc prog 1 64 2 21)) 0)
+
   (intcode-processor (vec '(1,9,10,3,2,3,11,0,99,30,40,50)))
   )
